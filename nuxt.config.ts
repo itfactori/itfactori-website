2e53f0cbdf764dfb1a9a2d83a14d8f37bc952647
affan-rehman <affanrehman1278@gmail.com>
2025-11-28 01:34:31 +0500

    fix conflicting rules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -29,10 +29,7 @@ export default defineNuxtConfig({
 
   eslint: {
     config: {
-      stylistic: {
-        commaDangle: 'never',
-        braceStyle: '1tbs'
-      }
+      stylistic: false
     }
   }
 });

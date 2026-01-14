aa54910953f60e4ff4634e8ba8f87528f52038d0
Raza Rauf <reachraz1@gmail.com>
2026-01-14 16:30:27 +0500

    chore lint

--- server/api/contact.ts
+++ server/api/contact.ts
@@ -212,5 +212,5 @@ function escapeHtml(text: string): string {
     '"': '&quot;',
     "'": '&#039;'
   };
-  return text.replace(/[&<>"']/g, m => map[m]);
+  return text.replace(/[&<>"']/g, m => map[m] || m);
 }
